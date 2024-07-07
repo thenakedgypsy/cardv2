f3cd180217014525649a3a03c22a9dbf42ab43cf
Sam <sam.chittenden@hotmail.com>
2024-07-07 14:35:36 +0100

    Added assets , basic card script

---
+++ Assets/scripts/cardScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cardScript : MonoBehaviour
+{
+
+    public string cardName;
+    public string description;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
